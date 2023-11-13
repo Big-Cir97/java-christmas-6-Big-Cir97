a3ed399d11e31977f3a73665fae37b004a1e6301
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-14 00:41:31 +0900

    feat: 상수 클래스를 abstract 클래스로 변경

--- src/main/java/christmas/utils/Constants.java
+++ src/main/java/christmas/utils/Constants.java
@@ -1,8 +1,14 @@
 package christmas.utils;
 
-public class Constants {
+public abstract class Constants {
 
     public static final int MIN_ORDER_MENU = 1;
     public static final int MIN_ORDER_QUANTITY = 1;
     public static final int MAX_ORDER_QUANTITY = 20;
+    public static final int GIVEAWAY_AMOUNT = 120_000;
+    public static final int NON_DISCOUNT = 0;
+
+    private Constants() {
+        throw new NullPointerException("[ERROR] 리플렉션을 제한합니다.");
+    }
 }
