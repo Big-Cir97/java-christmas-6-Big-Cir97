e854a7b8c231413408cf7249e43dd131367b587a
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-14 07:26:18 +0900

    feat: 증정 할인을 담당하는 class, test 추가

---
+++ src/main/java/christmas/model/discount/GiveawayDiscount.java
@@ -0,0 +1,24 @@
+package christmas.model.discount;
+
+
+import static christmas.enums.DiscountAmount.GIVEAWAY_DISCOUNT;
+import static christmas.enums.DiscountAmount.NON_DISCOUNT;
+
+import christmas.enums.MenuInfo;
+
+public class GiveawayDiscount implements Discount {
+
+    private final int totalOrderAmount;
+
+    public GiveawayDiscount(int totalOrderAmount) {
+        this.totalOrderAmount = totalOrderAmount;
+    }
+
+    @Override
+    public int calculateDiscount() {
+        if (totalOrderAmount >= GIVEAWAY_DISCOUNT.getDiscount()) {
+            return MenuInfo.CHRISTMAS_PASTA.getPrice();
+        }
+        return NON_DISCOUNT.getDiscount();
+    }
+}
---
+++ src/test/java/christmas/model/discount/GiveawayDiscountTest.java
@@ -0,0 +1,55 @@
+package christmas.model.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.model.order.MenuName;
+import christmas.model.order.MenuQuantity;
+import christmas.model.order.OrderDetail;
+import christmas.model.order.OrderMenu;
+import christmas.model.payment.Payment;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GiveawayDiscountTest {
+
+    private Payment payment;
+    private OrderDetail orderDetail;
+    private OrderMenu orderMenu;
+
+    @BeforeEach
+    void setup() {
+        payment = new Payment();
+        orderMenu = new OrderMenu();
+    }
+
+    @Test
+    @DisplayName("증정은 할인에 포함한다.")
+    void testDiscountGiveaway() {
+        orderMenu.addMenu(new MenuName("티본스테이크"), new MenuQuantity(10));
+        orderDetail = new OrderDetail(orderMenu);
+
+        int notDiscount = payment.beforeDiscountPayment(orderDetail);
+        GiveawayDiscount giveawayDiscount = new GiveawayDiscount(notDiscount);
+
+        int expected = 25_000;
+
+        assertThat(giveawayDiscount.calculateDiscount()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("증정이 없으면 할인되지 않는다.")
+    void testNonDiscountGiveaway() {
+        orderMenu.addMenu(new MenuName("아이스크림"), new MenuQuantity(1));
+        orderDetail = new OrderDetail(orderMenu);
+
+        int notDiscount = payment.beforeDiscountPayment(orderDetail);
+        GiveawayDiscount giveawayDiscount = new GiveawayDiscount(notDiscount);
+
+        int expected = 0;
+
+        assertThat(giveawayDiscount.calculateDiscount()).isEqualTo(expected);
+    }
+}
