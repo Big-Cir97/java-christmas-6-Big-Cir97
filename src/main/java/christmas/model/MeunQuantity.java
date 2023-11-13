8c294cc7a2e9df811b1902d4888c45198fbaeed2
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-13 14:45:28 +0900

    feat: 상수를 정의하는 Constants clss 추가

--- src/main/java/christmas/model/MeunQuantity.java
+++ src/main/java/christmas/model/MeunQuantity.java
@@ -1,9 +1,9 @@
 package christmas.model;
 
-public class MeunQuantity {
+import static christmas.utils.Constants.MAX_ORDER_COUNT;
+import static christmas.utils.Constants.MIN_ORDER_COUNT;
 
-    private static final int MIN_ORDER_COUNT = 1;
-    private static final int MAX_ORDER_COUNT = 20;
+public class MeunQuantity {
 
     private final int quantity;
 
@@ -16,25 +16,25 @@ public class MeunQuantity {
         return quantity;
     }
 
-    private void validate(int count) {
-        validateRange(count);
+    private void validate(int quantity) {
+        validateQuantityRange(quantity);
     }
 
-    private void validateRange(int count) {
-        if (checkMinCount(count)) {
+    private void validateQuantityRange(int quantity) {
+        if (checkMinQuantity(quantity)) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
 
-        if (checkMaxCount(count)) {
-            throw new IllegalArgumentException(String.format("[ERROR] 주문 수량은 최대 %d개 입니다. 다시 입력해 주세요.", MAX_ORDER_COUNT));
+        if (checkMaxQuantity(quantity)) {
+            throw new IllegalArgumentException(String.format("[ERROR] 주문 수량은 최대 %d개 입니다. 다시 입력해 주세요.", MIN_ORDER_COUNT));
         }
     }
 
-    private boolean checkMinCount(int count) {
-        return count < MIN_ORDER_COUNT;
+    private boolean checkMinQuantity(int quantity) {
+        return quantity < MIN_ORDER_COUNT;
     }
 
-    private boolean checkMaxCount(int count) {
-        return count > MAX_ORDER_COUNT;
+    private boolean checkMaxQuantity(int quantity) {
+        return quantity > MAX_ORDER_COUNT;
     }
 }
---
+++ src/main/java/christmas/utils/Constants.java
@@ -0,0 +1,7 @@
+package christmas.utils;
+
+public class Constants {
+
+    public static final int MIN_ORDER_COUNT = 1;
+    public static final int MAX_ORDER_COUNT = 20;
+}
