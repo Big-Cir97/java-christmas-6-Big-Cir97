eb636636d43230205bff1d61d423c5705ff4c5ef
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-13 14:58:45 +0900

    feat: 메뉴를 Category로 분류하는 enum class 추가

--- src/main/java/christmas/enums/MenuInfo.java
+++ src/main/java/christmas/enums/MenuInfo.java
@@ -4,32 +4,30 @@ import java.util.Arrays;
 
 public enum MenuInfo {
 
-    // 에피타이저
-    MUSHROOM_SOUP("양송이수프", 6_000),
-    TAPAS("타파스", 5_500),
-    CAESAR_SALAD("시저샐러드", 8_000),
+    MUSHROOM_SOUP("양송이수프", 6_000, Category.APPETIZER),
+    TAPAS("타파스", 5_500, Category.APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8_000, Category.APPETIZER),
 
-    // 메인
-    T_BONE_STEAK("티본스테이크", 55_000),
-    BARBECUE_RIBS("바비큐립", 54_000),
-    SEAFOOD_PASTA("해산물파스타", 35_000),
-    CHRISTMAS_PASTA("크리스마스파스타", 25_000),
+    T_BONE_STEAK("티본스테이크", 55_000, Category.MAIN_COURSE),
+    BARBECUE_RIBS("바비큐립", 54_000, Category.MAIN_COURSE),
+    SEAFOOD_PASTA("해산물파스타", 35_000, Category.MAIN_COURSE),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000, Category.MAIN_COURSE),
 
-    // 디저트
-    CHOCOLATE_CAKE("초코케이크", 15_000),
-    ICE_CREAM("아이스크림", 5_000),
+    CHOCOLATE_CAKE("초코케이크", 15_000, Category.DESSERT),
+    ICE_CREAM("아이스크림", 5_000, Category.DESSERT),
 
-    // 음료
-    ZERO_COLA("제로콜라", 3_000),
-    RED_WINE("레드와인", 60_000),
-    CHAMPAGNE("샴페인", 25_000);
+    ZERO_COLA("제로콜라", 3_000, Category.BEVERAGE),
+    RED_WINE("레드와인", 60_000, Category.BEVERAGE),
+    CHAMPAGNE("샴페인", 25_000, Category.BEVERAGE);
 
     private final String name;
     private final int price;
+    private final Category category;
 
-    MenuInfo(String name, int price) {
+    MenuInfo(String name, int price, Category category) {
         this.name = name;
         this.price = price;
+        this.category = category;
     }
 
     public String getName() {
