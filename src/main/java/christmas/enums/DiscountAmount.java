0f241c09945def2d3adcc394972f69162847edbf
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-14 06:02:41 +0900

    feat: 할인 element 추가

--- src/main/java/christmas/enums/DiscountAmount.java
+++ src/main/java/christmas/enums/DiscountAmount.java
@@ -5,6 +5,8 @@ public enum DiscountAmount {
     NON_DISCOUNT(0),
     DAY_DISCOUNT(100),
     SPECIAL_DISCOUNT(1_000),
+    MAIN_COURSE_DISCOUNT(2_023),
+    DESERT_DISCOUNT(2_023),
     GIVEAWAY_DISCOUNT(120_000);
 
     private final int discount;
