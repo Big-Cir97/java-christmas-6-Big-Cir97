29bb030afc5a14bf6cc162fcb84ca012e0cd6c07
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-14 05:41:35 +0900

    feat: 할인과 연관된 enum class 추가

---
+++ src/main/java/christmas/enums/DiscountAmount.java
@@ -0,0 +1,19 @@
+package christmas.enums;
+
+public enum DiscountAmount {
+
+    NON_DISCOUNT(0),
+    DAY_DISCOUNT(100),
+    SPECIAL_DISCOUNT(1_000),
+    GIVEAWAY_DISCOUNT(120_000);
+
+    private final int discount;
+
+    DiscountAmount(int discount) {
+        this.discount = discount;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+}
