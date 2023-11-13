363d1922cbcf97699587f2a4ef89b8009724fa61
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-14 06:41:11 +0900

    feat: 주말에 메인 요리 할인을 담당하는 class, test 추가

---
+++ src/main/java/christmas/model/discount/WeekendDiscount.java
@@ -0,0 +1,46 @@
+package christmas.model.discount;
+
+import static christmas.enums.DiscountAmount.NON_DISCOUNT;
+import static christmas.utils.Constants.MIN_ORDER_MENU;
+
+import christmas.enums.MenuInfo;
+import christmas.model.calendar.Calendar;
+import christmas.model.order.MenuName;
+import christmas.model.order.OrderDetail;
+import christmas.utils.Constants;
+import java.util.List;
+
+public class WeekendDiscount implements Discount {
+
+    private final Calendar calendar;
+    private final OrderDetail orderDetail;
+
+    public WeekendDiscount(Calendar calendar, OrderDetail orderDetail) {
+        this.calendar = calendar;
+        this.orderDetail = orderDetail;
+    }
+
+    @Override
+    public int calculateDiscount() {
+        if (calendar.isWeekend()) {
+            return mainCourseDiscountPrice();
+        }
+        return NON_DISCOUNT.getDiscount();
+    }
+
+    private int mainCourseDiscountPrice() {
+        List<MenuName> mainMenu = orderDetail.getMainMenu();
+        int totalPrice = NON_DISCOUNT.getDiscount();
+        if (mainMenu.size() < MIN_ORDER_MENU) {
+            return totalPrice;
+        }
+
+        for (MenuName menuName : mainMenu) {
+            int quantityByMenu = orderDetail.getQuantityByMenu(menuName);
+            int price = MenuInfo.findByMenuName(menuName.getName()).getPrice();
+            totalPrice += (quantityByMenu * price);
+        }
+
+        return totalPrice;
+    }
+}
---
+++ src/test/java/christmas/model/discount/WeekendDiscountTest.java
@@ -0,0 +1,52 @@
+package christmas.model.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.model.calendar.Calendar;
+import christmas.model.calendar.CalendarFactory;
+import christmas.model.order.MenuName;
+import christmas.model.order.MenuQuantity;
+import christmas.model.order.OrderDetail;
+import christmas.model.order.OrderMenu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WeekendDiscountTest {
+
+    @Test
+    @DisplayName("주말에는 메인 요리를 할인받는다.")
+    void testDiscountMainCourse() {
+        int visitDay = 1;
+        Calendar calendar = CalendarFactory.createCalendar(visitDay);
+
+        String mainCourse = "티본스테이크";
+        int quantity = 1;
+        OrderMenu orderMenu = new OrderMenu();
+        orderMenu.addMenu(new MenuName(mainCourse), new MenuQuantity(quantity));
+        OrderDetail orderDetail = new OrderDetail(orderMenu);
+
+        WeekendDiscount weekendDiscount = new WeekendDiscount(calendar, orderDetail);
+        int expected = 55_000;
+
+        assertThat(weekendDiscount.calculateDiscount()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("주문에 메인 요리가 없다면 할인 금액은 0원 이다.")
+    void testDiscountZero() {
+        int visitDay = 3;
+        Calendar calendar = CalendarFactory.createCalendar(visitDay);
+
+        String mainCourse = "시저샐러드";
+        int quantity = 1;
+        OrderMenu orderMenu = new OrderMenu();
+        orderMenu.addMenu(new MenuName(mainCourse), new MenuQuantity(quantity));
+        OrderDetail orderDetail = new OrderDetail(orderMenu);
+
+        WeekendDiscount weekendDiscount = new WeekendDiscount(calendar, orderDetail);
+        int expected = 0;
+
+        assertThat(weekendDiscount.calculateDiscount()).isEqualTo(expected);
+    }
+
+}
