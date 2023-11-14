9f6256a05319b11a448eb027bc58833bd81b38c0
Big-Cir97 <eodnjs01477@gmail.com>
2023-11-14 10:06:10 +0900

    feat: EventResult class 추가

---
+++ src/main/java/christmas/model/result/EventResult.java
@@ -0,0 +1,48 @@
+package christmas.model.result;
+
+import christmas.enums.BadgeInfo;
+import christmas.model.badge.Badge;
+
+public class EventResult {
+
+    private final DiscountResult discountResult;
+
+    public EventResult(DiscountResult discountResult) {
+        this.discountResult = discountResult;
+    }
+
+    public BadgeInfo getBadgeResult(Badge badge, int totalDiscountPrice) {
+        return badge.getBadgeName(totalDiscountPrice);
+    }
+
+    public int getChristmasDiscount() {
+        return discountResult.getChristmasDiscount();
+    }
+
+    public int getWeeksDaysDiscount() {
+        return discountResult.getWeeksDaysDiscount();
+    }
+
+    public int getWeekendDiscount() {
+        return discountResult.getWeekendDiscount();
+    }
+
+    public int getSpecialDiscount() {
+        return discountResult.getSpecialDiscount();
+    }
+
+    public int getGiveawayDiscount() {
+        return discountResult.getGiveawayDiscount();
+    }
+
+//    public int getChristmasDiscount() {
+//        return CHRISTMAS_DISCOUNT_RESULT + MINUS_OPERATOR + formmatComma() + RESULT_SUFFIX;
+//    }
+//
+//
+//
+//    private String formmatComma(int value) {
+//        DecimalFormat decimalFormat = new DecimalFormat(Constants.AMOUNT_FORMAT);
+//        return decimalFormat.format(value);
+//    }
+}
